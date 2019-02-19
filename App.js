f62d3218de9416d2fdeb61b4d88f0f992be1d656
ksut3955 <ksut3955@sysco.com>
2019-02-19 23:31:08 +0530

    a2

--- App.js
+++ App.js
@@ -45,7 +45,7 @@ export default class App extends React.Component {
 
   _handleLoadingError = error => {
     // In this case, you might want to report the error to your error
-    // reporting service, for example Sentry
+    // reporting service, for example Sentr
     console.warn(error);
   };
 
